878cfe0d5bf69ad8c3855af259ce3e8915cf187b
Victor Henriksson <vhenrixon@gmail.com>
2018-07-01 21:10:11 +0200

    +Add the first lessons

---
+++ Python/booleans.py
@@ -0,0 +1,5 @@
+# Making a varible called "number"
+number = 4
+# Is the number less then 5
+if number < 5:
+    print("I am less then 5")
---
+++ Python/dictionaries.py
@@ -0,0 +1,5 @@
+# Let create dictionary for some people ages
+age = {"Victor": 16, "John": 15, "Sarah": 16}
+# We can get data from a dictionary by 
+age['Sarah']
+# This would return 16 
